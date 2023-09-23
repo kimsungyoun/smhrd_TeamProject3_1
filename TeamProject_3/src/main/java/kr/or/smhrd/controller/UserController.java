22f9f071e8f61b5b31a729ad80ffbe7d0d7544b1
김성윤 <kimsy311@hanmail.net>
2023-09-23 17:32:18 +0900

    로그인 관련 dto, dao, service 설정 및 mapper 추가

--- TeamProject_3/src/main/java/kr/or/smhrd/controller/UserController.java
+++ TeamProject_3/src/main/java/kr/or/smhrd/controller/UserController.java
@@ -1,8 +1,11 @@
 package kr.or.smhrd.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,59 +18,60 @@ public class UserController {
 	@Autowired
 	UserService service;
 	
-	
 	//마이페이지로 이동
 	@GetMapping("/mypage")
-	String mypage() {
+	public String mypage() {
 		return "/register/mypage";
 	}
 	
 	//회원정보 수정폼으로 이동
 	@GetMapping("/userEdit")
-	String userEdit() {
-		return "/register/userEdit";
+	public ModelAndView userEdit(String u_id) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("dto", u_id);
+		mav.setViewName("register/userEdit");
+		return mav;
 	}
 	
 	//회원탈퇴 폼으로 이동
 	@GetMapping("/userResign")
-	String userWithdrawal() {
+	public String userResign() {
 		return "/register/userResign";
 	}
 	
 	//로그인 폼으로 이동
 	@GetMapping("/login")
-	String login() {
+	public String login() {
 		return "/register/login";
 	}
 	
 	//아이디 찾기 폼으로 이동
 	@GetMapping("/idSearch")
-	String idSearch() {
+	public String idSearch() {
 		return "/register/idSearch";
 	}
 	
 	//패스워드 찾기 폼으로 이동
 	@GetMapping("/pwSearch")
-	String pwSearch() {
+	public String pwSearch() {
 		return "/register/pwSearch";
 	}
 	
 	//회원가입 폼으로 이동
 	@GetMapping("/signUp")
-	String signUp() {
+	public String signUp() {
 		return "/register/signUp";
 	}
 		
-	// 로그인 확인
-	/*
-	public ModelAndView loginOk(String mem_id, String mem_password, HttpSession session) {
-		UserDTO dto = service.loginOk(mem_id, mem_password);
+	// 로그인
+	@PostMapping("/loginOk")
+	public ModelAndView loginOk(String u_id, String u_pw, HttpSession session) {
+		UserDTO dto = service.loginOk(u_id, u_pw);
 		
 		ModelAndView mav = new ModelAndView();
 		
 		if(dto != null) {
-			session.setAttribute("logId", dto.getMem_id());
-			session.setAttribute("logType", dto.getMem_type());
+			session.setAttribute("logId", dto.getU_id());
 			session.setAttribute("logStatus", "Y");
 			
 			 mav.setViewName("redirect:/");
@@ -76,5 +80,4 @@ public class UserController {
 		}
 		return mav;
 	}
-	*/
 }
